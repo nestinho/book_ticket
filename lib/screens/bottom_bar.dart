afe5afe36834b24685c0534fef4630adf6cfa11d
nestinho <nestory2008@gmail.com>
2023-01-26 15:52:25 +0300

    Upcoming flights section with bg blue

---
+++ lib/screens/bottom_bar.dart
@@ -0,0 +1,70 @@
+import 'package:book_ticket/screens/home_screen.dart';
+import 'package:fluentui_icons/fluentui_icons.dart';
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatefulWidget {
+  const BottomBar({super.key});
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  int _selectedIndex = 0;
+  static final List<Widget> _widgetOptions = <Widget>[
+    // const Text("Home"),
+     HomeScreen(),
+    const Text("Search"),
+    const Text("Ticket"),
+    const Text("Profile"),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    //_selectedIndex = index;
+    //print('${_selectedIndex}'); //for debugging
+    //print(_selectedIndex.toString()); //for debugging
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: const Text("My App"),
+      // ),
+      body: Center(
+        child: _widgetOptions[_selectedIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        elevation: 10,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.blueGrey,
+        type: BottomNavigationBarType.fixed,
+        unselectedItemColor: const Color(0xFF526480),
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
+              label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
+              label: "Search"),
+          BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
+              label: "Ticket"),
+          BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
+              label: "Profile")
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/ticket_view.dart
@@ -0,0 +1,144 @@
+import 'package:book_ticket/utils/app_layout.dart';
+import 'package:book_ticket/utils/app_styles.dart';
+import 'package:book_ticket/widgets/thick_container.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class TicketView extends StatelessWidget {
+  const TicketView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = AppLayout.getSize(context);
+    return SizedBox(
+      width: size.width,
+      // height: size.height,
+      // height: 200,
+      child:
+          // container bg blue part
+          Container(
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(21), topRight: Radius.circular(21)),
+          color: Color(0xFF526799),
+        ),
+        margin: const EdgeInsets.only(left: 16),
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "NYC",
+                        style:
+                            Styles.headLineStyle3.copyWith(color: Colors.white),
+                      ),
+                      const Spacer(),
+                      const ThickContainer(),
+                      Expanded(
+                          child: Stack(
+                        children: [
+                          SizedBox(
+                            height: 24,
+                            child: LayoutBuilder(
+                              builder: (BuildContext context,
+                                  BoxConstraints constraints) {
+                                return Flex(
+                                  direction: Axis.horizontal,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: List.generate(
+                                      (constraints.constrainWidth() / 6)
+                                          .floor(),
+                                      (index) => const SizedBox(
+                                            width: 3,
+                                            height: 1,
+                                            child: DecoratedBox(
+                                                decoration: BoxDecoration(
+                                                    color: Colors.white)),
+                                          )),
+                                );
+                              },
+                              // child:
+                            ),
+                          ),
+                          Center(
+                            child: Transform.rotate(
+                                angle: 1.5,
+                                child: const Icon(
+                                  Icons.local_airport_rounded,
+                                  color: Colors.white,
+                                )),
+                          )
+                        ],
+                      )),
+                      const ThickContainer(),
+                      const Spacer(), //same as Expanded(child: Container())
+                      Text(
+                        "LON",
+                        style:
+                            Styles.headLineStyle3.copyWith(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                  const Gap(3),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: 100,
+                        child: Text(
+                          "New York City",
+                          style: Styles.headLineStyle4
+                              .copyWith(color: Colors.grey),
+                        ),
+                      ),
+                      Text(
+                        "8H 30M",
+                        style:
+                            Styles.headLineStyle3.copyWith(color: Colors.grey),
+                      ),
+                      SizedBox(
+                        width: 100,
+                        child: Text(
+                          "London",
+                          textAlign: TextAlign.end,
+                          style: Styles.headLineStyle4
+                              .copyWith(color: Colors.grey),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+      // container bg red part
+      // Container(
+      //   color: const Color(0xFFF37B67),
+      //   child: Row(
+      //     children: const [
+      //       SizedBox(
+      //         height: 20,
+      //         width: 10,
+      //         child: DecoratedBox(
+      //           decoration: BoxDecoration(
+      //             borderRadius: BorderRadius.only(
+      //               topRight: Radius.circular(10),
+      //               bottomRight: Radius.circular(10),
+      //             ),
+      //           ),
+      //         ),
+      //       ),
+      //     ],
+      //   ),
+      // ),
+    );
+  }
+}
